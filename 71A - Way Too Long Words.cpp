6e9464dbbbbb9419d3ce1917ce29ff1dd254abca
OGDubjazz <omar__yousry@hotmail.com>
2016-07-24 04:36:29 +0200

    Create 71A - Way Too Long Words.cpp

---
+++ 71A - Way Too Long Words.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {  
+    int n;
+    cin >> n;
+    string s;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        if (s.length() <= 10) {
+            cout << s << "\n";
+        } else {
+            cout << s[0] << s.length() - 2 << s[s.length() - 1] << "\n";
+        }
+    }
+    return 0;
+}
