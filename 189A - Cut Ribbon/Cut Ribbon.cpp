f8c83825aa75a1988400a1ebbd7c5e51bf33c60f
OGDubjazz <omar__yousry@hotmail.com>
2016-07-25 03:08:14 +0200

    Update Cut Ribbon.cpp

--- 189A - Cut Ribbon/Cut Ribbon.cpp
+++ 189A - Cut Ribbon/Cut Ribbon.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-	int n, a, b, c, ribbon = 0;
+	int n, a, b, c, k, ribbon = 0;
 	cin >> n >> a >> b >> c;
 
-	for (int i = 0; i*a <= n; ++i)
+	for (int i = 0; i*a <= n; i++)
 	{
-		for (int j = 0; i*a + j*b <= n; ++j)
+		for (int j = 0; i*a + j*b <= n; j++)
 		{
-			int k = (n - i*a - j*b)/c;
+			k = (n - i*a - j*b)/c;
 			if (i*a + j*b + k*c == n)
 			ribbon = max(i + j + k, ribbon);
 		
