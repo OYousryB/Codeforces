71c1e91adf9a632d711f75da79124645b22d03d6
OGDubjazz <omar__yousry@hotmail.com>
2016-08-18 14:24:58 +0200

    Update Beru-taxi.cpp

--- 706A - Beru-taxi/Beru-taxi.cpp
+++ 706A - Beru-taxi/Beru-taxi.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include<iomanip> //
 using namespace std;
 const int INF=0x3f3f3f;
 int mx,my,n,x[1005],y[1005],v[1005];
@@ -14,22 +15,8 @@ int main()
 		cin >> x[i] >> y[i] >> v[i];
 		ans=min(ans,sqrt((x[i]-mx)*(x[i]-mx)+(y[i]-my)*(y[i]-my))/v[i]);
 	}
-	cout.precision(20);
-	cout << ans << endl;
+	cout.setf(ios::fixed);    // printf("%.20lf\n",ans);
+	cout << setprecision(22)<<ans << endl;
 	return 0;
 }
 
-/*
-int main()
-{
-	scanf("%d%d",&sx,&sy);
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
-	{
-		scanf("%d%d%d",&x[i],&y[i],&v[i]);
-		ans=min(ans,sqrt((x[i]-sx)*(x[i]-sx)+(y[i]-sy)*(y[i]-sy))/v[i]);
-	}
-	printf("%.20lf\n",ans);
-	return 0;
-}
-*/
